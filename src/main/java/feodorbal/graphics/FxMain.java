0262a612b354c938b67de4049d64124f586df66f
fedxder <fed@gmail.com>
2024-09-25 21:44:39 +0600

    ...

---
+++ src/main/java/feodorbal/graphics/FxMain.java
@@ -0,0 +1,11 @@
+package feodorbal.graphics;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FxMain extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
